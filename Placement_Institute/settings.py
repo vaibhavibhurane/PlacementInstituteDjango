eda366f5bd83f0bacc02687fba6c9bd79aaabd9e
vikram.kumar <vikram.kumar@systematixindia.com>
2024-06-19 02:52:01 +0530

    changes on setting and urls file for files handling

--- Placement_Institute/settings.py
+++ Placement_Institute/settings.py
@@ -111,12 +111,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR, "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = [BASE_DIR, 'media']
